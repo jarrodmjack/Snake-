0dd6fecf22c809d83909755cfab901c619d61180
jarrodmjack <jarrodmjack@gmail.com>
2022-05-30 22:39:19 -0400

    created randomly generated food items

--- snake_game.js
+++ snake_game.js
@@ -1,10 +1,17 @@
+
+// Declare global variables to track game board size
 const LINE_PIXEL_COUNT = 40
 const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT ** 2
 
+
+// Track scores to display to user
 let totalFoodEaten = 0
 let totalDistanceTraveled = 0
 
+
+// Shorten reference to game board
 const gameContainer = document.querySelector('#gameContainer')
+
 
 // Creating the game board pixels
 const createGameBoardPixels = () => {
@@ -12,3 +19,28 @@ const createGameBoardPixels = () => {
         gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameBoardPixel" id="pixel${i}"></div>`
     }
 }
+
+
+// Shorten references to game pixels
+const gameBoardPixels = document.getElementsByClassName('gameBoardPixel')
+
+let currentFoodPosition = 0
+
+// create randomly generated food items on the game board
+const createFood = () => {
+    gameBoardPixels[currentFoodPosition].classList.remove('food')
+    currentFoodPosition = Math.floor(Math.random() * TOTAL_PIXEL_COUNT)
+    gameBoardPixels[currentFoodPosition].classList.add('food')
+}
+
+//Start setting up snake behavior
+
+const LEFT_DIR = 37
+const UP_DIR = 38
+const RIGHT_DIR = 39
+const DOWN_DIR = 40
+
+let snakeCurrentDirection = RIGHT_DIR
+
+
+
